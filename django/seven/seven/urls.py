b30fec8c86d1608f65813eb952d0dbe082b59e77
gomesmam <gomes.mam@gmail.com>
2014-11-23 04:11:00 -0500

    add urls & views

--- django/seven/seven/urls.py
+++ django/seven/seven/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'seven.views.home', name='home'),
+    url(r'^$', 'profiles.views.home', name='home'),
+    url(r'^about/', 'profiles.views.about', name='about'),
+    url(r'^contact/', 'profiles.views.contact', name='contact'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
