0e1e241182e84469b7b467fed40d4988773a4376
gomesmam <gomes.mam@gmail.com>
2014-11-23 04:58:40 -0500

    add static files

--- django/seven/seven/settings.py
+++ django/seven/seven/settings.py
@@ -81,8 +81,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'root')
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), 'static', 'static'),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), 'static', 'templates'),
