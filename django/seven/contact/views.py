ef5abbcfdb20338b8b8369c346e79e9287fee789
gomesmam <gomes.mam@gmail.com>
2014-11-26 11:49:38 -0500

    mudancas

--- django/seven/contact/views.py
+++ django/seven/contact/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
 from .forms import ContactForm
+from django.core.mail import send_mail
 
-# Create your views here.
 
 
 
@@ -10,8 +9,27 @@ from .forms import ContactForm
 def home(request):
     form = ContactForm(request.POST or None)
 
+    comment = form.cleaned_date['comment']
+    name = form.cleaned_date['name']
+    sbj = 'Message from Seven.com'
+    msg = '%s %s '  %(comment, name)
+    frm = form.cleaned_date['email']
+    to_us = [settings.EMAIL_HOST_USER]
+    print sbj, msg, frm, to_us
+    send_mail(sbj, msg, frm, ['mail@miguelgomes.eu'], fail_silently=False)
+
     if form.is_valid():
-        print request.POST
+        comment = form.cleaned_date['comment']
+        name = form.cleaned_date['name']
+        sbj = 'Message from Seven.com'
+        msg = '%s %s '  %(comment, name)
+        frm = form.cleaned_date['email']
+        to_us = [settings.EMAIL_HOST_USER]
+        print sbj, msg, frm, to_us
+        send_mail(sbj, msg, frm,
+            to_us, fail_silently=False)
+    else:
+        print "FORM INVALID"
 
     context = locals()
     template = "contact.html"
