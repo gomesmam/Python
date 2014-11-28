031272954c22d1f17ca4226e096ba643432958ac
gomesmam <gomes.mam@gmail.com>
2014-11-28 16:36:22 +0000

    mail funcional

--- django/seven/contact/views.py
+++ django/seven/contact/views.py
@@ -11,20 +11,29 @@ def home(request):
     form = ContactForm(request.POST or None)
     confirm_message = None
 
-    print form
-
     if form.is_valid():
+        # comment = form.cleaned_data['comment']
+        # name = form.cleaned_data['name']
+        # sbj = 'Message from Seven.com'
+        # msg = '%s %s '  %(comment, name)
+        # frm = form.cleaned_data['email']
+        # # to_us = [settings.EMAIL_HOST_USER]
+        # print sbj, msg, frm #, to_us
+        # send_mail(sbj, msg, frm, ['mail@miguelgomes.eu'], fail_silently=False)
+
         comment = form.cleaned_data['comment']
         name = form.cleaned_data['name']
-        email = form.cleaned_data['email']
+        frm  = form.cleaned_data['email']
         sbj = 'Message from Seven.com'
-        msg = '%s %s'  %(comment, name)
-        #frm = ['teste@3es.in']
-        to_us = ['teste@3es.in']
-        print sbj, msg, to_us
-        send_mail(sbj, msg, to_us, to_us, fail_silently=False)
+        msg = '%s %s %s'  %(comment, name, frm)
+        # #frm = ['teste@3es.in']
+        # to_us = ['teste@3es.in']
+        # print sbj, msg, to_us
+        # send_mail(sbj, msg, to_us, to_us, fail_silently=False)
+        send_mail(sbj, msg, 'teste@3es.in', ['mail@miguelgomes.eu'], fail_silently=False)
         title = "Thank You"
         confirm_message = " Thanks for message "
+
     else:
         print "FORM INVALID"
 
